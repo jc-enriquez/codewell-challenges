6d2701981d48e4387b928cd7bf119d1b6763d74e
jc-enriquez <john.christopher.enriquez2@gmail.com>
2023-12-10 14:30:17 +0800

    added prevent scroll

--- projects/snipper-landing-page/assets/js/script.js
+++ projects/snipper-landing-page/assets/js/script.js
@@ -1,7 +1,14 @@
+"use strict";
+
 const btnMobile = document.querySelector(".btn--mobile");
 const header = document.querySelector(".header");
 
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+
 const showMobileNav = function () {
+  html.classList.toggle("u-prevent-scroll");
+  body.classList.toggle("u-prevent-scroll");
   header.classList.toggle("active-mobile");
 };
 

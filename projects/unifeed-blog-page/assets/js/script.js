17622a0b08e3d87fbea9c5d1bad3521fdcfe9484
jc-enriquez <john.christopher.enriquez2@gmail.com>
2023-12-10 14:34:37 +0800

    added prevent scroll

--- projects/unifeed-blog-page/assets/js/script.js
+++ projects/unifeed-blog-page/assets/js/script.js
@@ -2,8 +2,12 @@
 
 const nav = document.querySelector(".navigation");
 const btnMobile = document.querySelector(".btn-mobile");
+const html = document.querySelector("html");
+const body = document.querySelector("body");
 
 const showMobileNav = function () {
+  html.classList.toggle("u-prevent-scroll");
+  body.classList.toggle("u-prevent-scrol");
   nav.classList.toggle("active");
 };
 

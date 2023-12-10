7c03399f34c715d286fb5bcbde5768f18c4d3e59
jc-enriquez <john.christopher.enriquez2@gmail.com>
2023-12-10 14:24:31 +0800

    disabled scroll when mobile nav is open

--- projects/commune-waitlist-page/assets/js/script.js
+++ projects/commune-waitlist-page/assets/js/script.js
@@ -2,9 +2,14 @@
 
 const btnNav = document.querySelector(".btn-nav");
 const nav = document.querySelector(".navigation");
+const html = document.querySelector("html");
+const body = document.querySelector("body");
 
 const showMobileNav = function () {
+  html.classList.toggle("prevent-scroll");
+  body.classList.toggle("prevent-scroll");
   nav.classList.toggle("active");
+  console.log(body);
 };
 
 btnNav.addEventListener("click", showMobileNav);

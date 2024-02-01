93225a4866c10f1d56898b628745dfc63d0e90f8
jc-enriquez <john.christopher.enriquez2@gmail.com>
2024-02-01 16:36:07 +0800

    fixed mobile responsiveness

--- projects/developer-portfolio/assets/js/script.js
+++ projects/developer-portfolio/assets/js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const btnMobile = document.querySelector(".btn--mobile");
+const headerEl = document.querySelector(".header__content");
+
+btnMobile.addEventListener("click", function () {
+  headerEl.classList.toggle("active");
+});

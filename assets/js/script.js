bb4009c017f86b0f1a1abb60ddc4394536630695
jc-enriquez <john.christopher.enriquez2@gmail.com>
2024-02-03 04:25:16 +0800

    implemented fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,100 +51,20 @@ window.addEventListener("scroll", watchScroll);
 btnScroll.addEventListener("click", scrollToTop);
 
 // Display Projects
-const projects = [
-  {
-    title: "Spense Landing Page",
-    projLink: "projects/spense-landing-page/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/spense-landing-page",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/spense-landing-page.webp",
-    skills: ["HTML", "CSS", "SASS", "JavaScript"],
-  },
-  {
-    title: "Fiber Landing Page",
-    projLink: "projects/fiber-landing-page/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/fiber-landing-page",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/fiber-landing-page.webp",
-    skills: ["HTML", "CSS", "SASS", "JavaScript"],
-  },
-  {
-    title: "Gradie Sign Up Page",
-    projLink: "projects/gradie-signup-page/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/gradie-signup-page",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/gradie-sign-up-page.webp",
-    skills: ["HTML", "CSS", "SASS"],
-  },
-  {
-    title: "IndieBrew Landing Page",
-    projLink: "projects/indiebrew-landing-page/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/indiebrew-landing-page",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/indiebrew-landing-page.webp",
-    skills: ["HTML", "CSS", "SASS", "JavaScript"],
-  },
-  {
-    title: "Snipper Landing Page",
-    projLink: "projects/snipper-landing-page/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/snipper-landing-page",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/snipper-landing-page.webp",
-    skills: ["HTML", "CSS", "SASS", "JavaScript"],
-  },
-  {
-    title: "Comment Module",
-    projLink: "projects/comment-module/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/comment-module",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/comment-module.webp",
-    skills: ["HTML", "CSS", "SASS"],
-  },
-  {
-    title: "Commune Waitlist Page",
-    projLink: "projects/commune-waitlist-page/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/commune-waitlist-page",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/commune-waitlist-page.webp",
-    skills: ["HTML", "CSS", "SASS", "JavaScript"],
-  },
-  {
-    title: "TimeNow Landing Page",
-    projLink: "projects/timenow-landing-age/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/timenow-landing-page",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/timenow-landing-page.webp",
-    skills: ["HTML", "CSS", "SASS", "JavaScript"],
-  },
-  {
-    title: "Unifeed Blog Page",
-    projLink: "projects/unifeed-blog-page/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/unifeed-blog-page",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/unifeed-blog-page.webp",
-    skills: ["HTML", "CSS", "SASS", "JavaScript"],
-  },
-  {
-    title: "Newsletter Popup Template",
-    projLink: "projects/newsletter-popup-template/",
-    codeLink:
-      "https://github.com/jc-ve/codewell-challenges/tree/main/projects/newsletter-popup-template",
-    imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/codewell/newsletter-popup-template.webp",
-    skills: ["HTML", "CSS", "SASS"],
-  },
-];
+const getProject = () => {
+  fetch("https://cdn.jsdelivr.net/gh/jc-ve/projects-data@latest/codewell.json")
+    .then((res) => res.json())
+    .then((data) => {
+      displayProjects(data);
+    })
+    .catch((err) => {
+      containerProject.innerHTML = `<p>No data retrieved. ${err}</p>`;
+    });
+};
 
-const displayProjects = () => {
+getProject();
+
+const displayProjects = (projects) => {
   projects.forEach(({ title, projLink, codeLink, imgLink, skills }) => {
     let skillsContainer = [];
 
@@ -179,7 +99,7 @@ const displayProjects = () => {
     </div>
   </div>
     `;
-    containerProject.insertAdjacentHTML("afterbegin", html);
+    containerProject.insertAdjacentHTML("beforeend", html);
   });
 };
 displayProjects();

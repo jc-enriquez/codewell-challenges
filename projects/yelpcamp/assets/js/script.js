79ab41b68fef78dddf626db5086c43c056bf6a52
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-02-28 22:31:32 +0800

    fixed mobile navigation

--- projects/yelpcamp/assets/js/script.js
+++ projects/yelpcamp/assets/js/script.js
@@ -1,9 +1,20 @@
 "use strict";
 
 const banner = document.querySelector(".banner");
+const navigation = document.querySelector(".navigation");
 const btnCloseBanner = document.querySelector(".banner__btn");
+const btnMobile = document.querySelector(".btn--mobile");
 
 btnCloseBanner.addEventListener("click", function () {
   banner.classList.add("hidden");
-  console.log("hello");
+});
+
+btnMobile.addEventListener("click", function () {
+  navigation.classList.toggle("active");
+});
+
+window.addEventListener("scroll", function () {
+  if (navigation.classList.contains("active")) {
+    navigation.classList.remove("active");
+  }
 });
